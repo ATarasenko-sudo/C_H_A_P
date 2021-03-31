9a484400b0bcabd5d866b6fa44b2c1d0ffaca128
ATarasenko-sudo <andrey.tarasenko.00@list.ru>
2021-03-31 12:24:07 +0300

    task B.1 prototype

---
+++ diff_robot/scripts/robot_sim_drive.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Header
+
+
+def enc_vals(data)
+    L = 20
+    r = 5
+    V = data.linear.x #or y
+    w = data.angular.z
+    w_l = (2*V + w*L)/(2*r)
+    w_r = (2*V + r*w_l)/r
+    w_rl = [w_r, w_l]
+    return w_rl
+
+
+def talker(data):
+    rospy.init_node('enc_pub')
+    pub = rospy.Publisher("encoder_data", Header)
+    msg = Header()
+    msg.head.stamp = rospy.Time.now()
+    msg.encoder_r = enc_r
+    msg.encoder_l = enc_l
+    pub.publish(msg)
+
+
+def listener():
+    rospy.init_node("listener")
+    sub = = rospy.Subscriber("cmd_vel",Twist, talker)
+    
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    listener()
+
+
+
+    
+
